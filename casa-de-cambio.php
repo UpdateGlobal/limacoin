d9eb7a57b75b62d1fb2cf5d512388d1cce12d90d
Frontend Update <arte@update.pe>
2018-03-28 13:21:35 -0500

    luis

--- casa-de-cambio.php
+++ casa-de-cambio.php
@@ -12,9 +12,66 @@ include 'includes/head.php'
 
     <div class="page-wraper">
         	
-             <?
-     include 'includes/header.php'
-     ?>
+        <header class="site-header header-style-3 topbar-transparent">
+            <div class="top-bar">
+                <div class="container  hidden-xs hidden-sm">
+                    <div class="row">
+                        <div class="clearfix">
+                            <div class="wt-topbar-left">
+                                <ul class="list-unstyled e-p-bx pull-left">
+                                    <li><i class="fa fa-envelope"></i>mail@limacoin.com</li>
+                                    <li><i class="fa fa-phone"></i>(511) 321-7654</li>
+                                </ul>
+                            </div>
+                            
+                            <div class="wt-topbar-right">
+                                    <ul class="social-icons social-square social-darkest">
+                                    <li><i class="fa fa-envelope"></i>Síguenos en: &nbsp</li>
+                                    <li><a href="javascript:void(0);" class="fa fa-facebook"></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="sticky-header main-bar-wraper">
+                <div class="main-bar">
+                    <div class="container">
+                        
+                            <div class="logo-header mostion">
+                                <a href="index.php">
+                                    <img src="images/logo-light.png" width="230" height="67" alt="" />
+                                </a>
+                            </div>
+
+                            <button data-target=".header-nav" data-toggle="collapse" type="button" class="navbar-toggle collapsed">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+
+                            <div class="header-nav navbar-collapse collapse smooth-scroll">
+                            <ul class=" nav navbar-nav smooth-scroll">
+                                <li class="active"><a href="index.php#inicio">Inicio</a></li>
+                                <li><a href="index.php#servicios">Servicios<i class="fa fa-chevron-down"></i></a>
+                                    <ul class="sub-menu">
+                                        <li><a href="casa-de-cambio.php">Casa de Cambio</a></li>
+                                        <li><a href="mineria-cloud.php">Minería Cloud</a></li>    
+                                    </ul>
+                                </li>
+                                <li><a href="index.php#nosotros">Nosotros</a></li>
+                                <li><a href="index.php#limacoin">Porqué Lima Coin</a></li>
+                                <li class="submenu-direction"><a href="index.php#contacto">Contacto</a></li>
+                            </ul>
+                        </div>
+        
+                    </div>
+                </div>
+            </div>
+        </header>
+
             
         <!-- CONTENT START -->
         <div class="page-content">
