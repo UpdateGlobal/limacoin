e96293db92a235feb8718faf852a57899d4b1d8a
luizink <luizupdate@gmail.com>
2018-04-16 16:17:55 -0500

    estadíscticas

---
+++ includes/estadisticas.php
@@ -0,0 +1,39 @@
+<!-- NUMEROS Y ESTADISTICAS -->
+            <div class="section-full bg-primary p-t50 p-b30">
+                <div class="container">
+                    <div class="section-content">
+                        <div class="row">
+                                
+                            <div class="col-md-3 col-sm-6">
+                                <div class="text-black wt-icon-box-wraper center">
+                                    <div class="counter font-70 font-weight-800 m-b5">2009</div>
+                                    <span class="font-18">Inicio de Bitcoin</span>
+                                </div>
+                            </div>
+                            
+                            <div class="col-md-3 col-sm-6">
+                                <div class="m-b30 text-black wt-icon-box-wraper center">
+                                    <div class="font-70 font-weight-800 m-b5"><span class="counter">1,335</span></div>
+                                    <span class="font-18">Criptodivisas</span>
+                                </div>
+                            </div>
+                            
+                            <div class="col-md-3 col-sm-6">
+                                <div class="m-b30 text-black wt-icon-box-wraper center">
+                                    <div class="counter font-70 font-weight-800 m-b5"> 572,000</div>
+                                    <span class="font-18">USD$ Millones</span>
+                                </div>
+                            </div>
+                            
+                            <div class="col-md-3 col-sm-6">
+                                <div class="m-b30 wt-icon-box-wraper center text-black">
+                                    <div class="counter font-70 font-weight-800 m-b5">1000</div>
+                                    <span class="font-18">% Revalorización</span>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- NUMEROS Y ESTADISTICAS --> 
--- mineria-cloud.php
+++ mineria-cloud.php
@@ -170,45 +170,9 @@ include 'includes/head.php'
             </div>   
             <!-- DETALLE DE SERVICIO END --> 
 
-             <!-- NUMEROS Y ESTADISTICAS -->
-            <div class="section-full bg-primary p-t50 p-b30">
-                <div class="container">
-                    <div class="section-content">
-                        <div class="row">
-                                
-                            <div class="col-md-3 col-sm-6">
-                                <div class="text-black wt-icon-box-wraper center">
-                                    <div class="counter font-70 font-weight-800 m-b5">35</div>
-                                    <span class="font-18">Support Countries</span>
-                                </div>
-                            </div>
-                            
-                            <div class="col-md-3 col-sm-6">
-                                <div class="m-b30 text-black wt-icon-box-wraper center">
-                                    <div class="font-70 font-weight-800 m-b5"><span class="counter">700</span></div>
-                                    <span class="font-18">BitCoin ATMs</span>
-                                </div>
-                            </div>
-                            
-                            <div class="col-md-3 col-sm-6">
-                                <div class="m-b30 text-black wt-icon-box-wraper center">
-                                    <div class="counter font-70 font-weight-800 m-b5">300</div>
-                                    <span class="font-18">Producers</span>
-                                </div>
-                            </div>
-                            
-                            <div class="col-md-3 col-sm-6">
-                                <div class="m-b30 wt-icon-box-wraper center text-black">
-                                    <div class="counter font-70 font-weight-800 m-b5">55</div>
-                                    <span class="font-18">Operators</span>
-                                </div>
-                            </div>
-                            
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- NUMEROS Y ESTADISTICAS --> 
+             <?
+             include 'includes/estadisticas.php'
+             ?>
 
             <!-- PORQUÉ INVERTIR EN BITCOIN  -->
             <div class="section-full  p-tb80 bg-gray">
